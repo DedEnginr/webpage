fd4a1a1aefb8c3e1b38c7074bdf9bfe680c431eb
DedEnginr <hulksmashed1100@gmail.com>
2025-07-22 20:38:32 +0530

    Finalized vercel/analytics configuration.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Index from './pages/Index';
 import Contact from './pages/Contact';
 import NotFound from './pages/NotFound';
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const queryClient = new QueryClient();
 
